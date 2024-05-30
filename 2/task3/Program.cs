b96b774d73686e14814da7a8bb8543b9897c80dc
Xan <test@mail.ru>
2024-05-30 21:35:21 +0500

    Add some tasks

---
+++ 2/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr = { 2, 3, 4, 5, 7, 5, 6, 7, 89, 4 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i++;
+}
+
+
