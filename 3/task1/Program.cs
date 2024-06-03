9bdc4b17e09f7103961111b4922327dc8c8834c4
Xan <test@mail.ru>
2024-06-03 20:58:43 +0500

    Lesson 3 task1

---
+++ 3/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+double calcFunc(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    double denomenator = c + d;
+
+    double res = numenator / denomenator;
+    return res;
+};
+
+Console.WriteLine(calcFunc(2, 3, 4, 5));
+
