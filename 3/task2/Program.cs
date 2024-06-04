766e6061afa6b827441347457b0a934f54d66eee
Xan <test@mail.ru>
2024-06-04 22:38:16 +0500

    lesson 3. task2, task3

---
+++ 3/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿void printSquare(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+printSquare(2);
+printSquare(3);
+printSquare(4);
