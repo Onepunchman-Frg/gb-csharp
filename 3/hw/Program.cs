486ee589088d12c62f545dc92862789f22abc90f
Xan <test@mail.ru>
2024-06-10 02:53:10 +0500

    lesson 3 and 4

---
+++ 3/hw/Program.cs
@@ -0,0 +1,48 @@
+﻿int n = 10;
+int[] arr = new int[n];
+
+fillArray(arr);
+printArray(arr);
+
+int sum = sumArr(arr);
+int product = prodArr(arr);
+
+Console.WriteLine();
+Console.WriteLine(sum);
+Console.WriteLine(product);
+
+void fillArray(int[] inArr) {
+    int i = 0;
+    while (i < inArr.Length) {
+        inArr[i] = i + 1;
+        i++;
+    }
+}
+
+void printArray(int[] inArr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int sumArr(int[] inArr) {
+    int i = 0;
+    int sum = 0;
+    while (i < inArr.Length) {
+        sum += inArr[i];
+        i++;
+    }
+    return sum;
+}
+
+int prodArr(int[] inArr) {
+    int i = 0;
+    int product = 1;
+    while (i < inArr.Length) {
+        product *= inArr[i];
+        i++;
+    }
+    return product;
+}
---
+++ 4/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] createTable(int rowCount, int colCount)
+{
+    int[,] table = new int[rowCount, colCount];
+    Random rnd = new Random();
+
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return table;
+}
+
+void showTable(int[,] inputTable)
+{
+    for (int i = 0; i < inputTable.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputTable.GetLength(1); j++)
+        {
+            Console.Write($"{inputTable[i, j]} ");
+        }
+        Console.WriteLine($" ");
+    }
+}
+
+
+int[,] talbeTest = createTable(4,8);
+showTable(talbeTest);
